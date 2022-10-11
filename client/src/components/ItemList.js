90d52008c0ded179a8d4cb9485f112fa9390437c
gokhandogan7 <gdogan3746@gmail.com>
2022-10-11 09:11:21 +0200

    useDebounce hook implemented for debouncing time in search

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -3,23 +3,28 @@ import { useApi } from "../hooks/useApi";
 import { services } from "../services";
 import Item from "./ui/Item";
 import "./itemlist.css";
+import useDebounce from "../hooks/useDebounce";
+import SearchBox from "./ui/SearchBox";
+
 
 function ItemList() {
   const { state } = useApi(services.getArticles, [], false);
-  const [query, setQuery] = useState("");
+  const [value, setValue] = useState('')
+  const debouncedValue = useDebounce(value, 1500)
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1 style={{ textAlign: "center" }}>List of Data</h1>
-      <input
-       className="inputBox"
-        placeholder="Search Items"
-        onChange={(event) => setQuery(event.target.value)}
-      />
-      
-      <Item items={state} search={query} />
+      <SearchBox handleChange={handleChange} value={value}/>
+      <Item items={state} search={debouncedValue} />
     </div>
   );
 }
+
+
 
 export default ItemList;
