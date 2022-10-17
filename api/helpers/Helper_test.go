cdaa4d6a5bee8b7c18ab3fac1ae7912f1b426e08
gokhandogan7 <gdogan3746@gmail.com>
2022-10-17 11:26:48 +0200

    code clean up

--- api/helpers/Helper_test.go
+++ api/helpers/Helper_test.go
@@ -7,19 +7,17 @@ import (
 
 func TestIsSearched(t *testing.T) {
 
-	expected := true
-
 	var TestArticle = entities.Article{
 		Id:      0,
-		Title:   "SEArchkey ",
+		Title:   "SEAr chkey ",
 		Desc:    "testDEscription",
 		Context: "test content",
 	}
 
 	result := IsSearched(TestArticle, "searchkey")
 
-	if expected != result {
-		t.Errorf("handler returned wrong status code: got %v want %v", result, expected)
+	if result != true {
+		t.Errorf("handler returned wrong status code: got %v want %v", result, true)
 	}
 
 }
