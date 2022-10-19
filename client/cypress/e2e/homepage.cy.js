f7763dff943fe90b21f8efbb42346e75ce23452d
gokhandogan7 <gdogan3746@gmail.com>
2022-10-19 14:23:32 +0200

    more e2e tests added

--- client/cypress/e2e/homepage.cy.js
+++ client/cypress/e2e/homepage.cy.js
@@ -1,18 +1,22 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress"/>
 describe("App Component", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+  before(() => {
+    cy.visit("/");
   });
 
   it("goes to the home page", () => {
     cy.get(".header").should("exist");
     cy.get(".motto").should("exist");
-  });
-
-  it("routes to details page", () => {
+    cy.get(".link").should("exist");
     cy.get(".link").click();
     cy.url().should("include", "/details");
-    cy.get(".inputBox").should("have.value", '')
+    cy.request("/details").should("exist", ".inputBox");
+    cy.get(".inputBox").should("have.value", "");
+    cy.get(".inputBox").type("4");
+    cy.get(".title").findByText("title 4").should("exist")
+    cy.get(".description").findByText("description 4").should("exist")
+    cy.get(".content").findByText("content 4").should("exist")
+    
   });
 });
